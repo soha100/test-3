77eb515b0108e8ea292d150f2a26b986fc99dce9
soha100 <sohaahmed718@gmail.com>
2024-06-29 00:24:37 +0300

    update

---
+++ main.js
@@ -0,0 +1,21 @@
+// // احصل على جميع عناصر الـ container
+// var containers = document.querySelectorAll('.landing .container');
+// var currentIndex = 0;
+
+// // اضف معالج الحدث للضغط على السهم الأيمن
+// document.querySelector('.arrow.right').addEventListener('click', function() {
+//   if (currentIndex < containers.length - 1) {
+//     containers[currentIndex].style.display = 'none';
+//     containers[currentIndex + 1].style.display = 'block';
+//     currentIndex++;
+//   }
+// });
+
+// // اضف معالج الحدث للضغط على السهم الأيسر
+// document.querySelector('.arrow.left').addEventListener('click', function() {
+//   if (currentIndex > 0) {
+//     containers[currentIndex].style.display = 'none';
+//     containers[currentIndex - 1].style.display = 'block';
+//     currentIndex--;
+//   }
+// });
